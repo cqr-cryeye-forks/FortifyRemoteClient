54fc0e977c3f22748c8ac25666bf71e5922de67b
Николай Борщёв <56702121+zeinlol@users.noreply.github.com>
2022-05-12 00:21:07 +0200

    fix output

--- core/files/output.py
+++ core/files/output.py
@@ -5,15 +5,17 @@ from dict2xml import dict2xml
 
 
 def save_result(result: bytes, output_path: Path):
-    if output_path.suffix == '.json':
+    if output_path.suffix in ['.xml', 'fvdl', '.json']:
         result = json.loads(result)
         with open(output_path, 'w') as file:
-            json.dump(result, file, indent=2)
+            if output_path.suffix in ['.xml', 'fvdl']:
+                result = dict2xml(result)
+                file.write(result)
+            else:
+                json.dump(result, file, indent=2)
             file.close()
     else:
         with open(output_path, 'wb') as file:
-            if output_path.suffix in ['.xml', 'fvdl']:
-                result = dict2xml(result)
             file.write(result)
             file.close()
     print(f'Data saved to {output_path.absolute()}')
