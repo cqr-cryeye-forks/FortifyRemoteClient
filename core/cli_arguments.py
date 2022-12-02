76a03f89bcd1991672c4bd38106b0d28f660a656
zeinlol <nickolaiborshchov@gmail.com>
2022-12-02 16:15:42 +0000

    disable default target value

--- core/cli_arguments.py
+++ core/cli_arguments.py
@@ -1,15 +1,14 @@
 import argparse
 from pathlib import Path
 
-from core.data.config import PARENT_BASE, BASE_PATH
+from core.data.config import BASE_PATH
 
 
 def create_parser():
     parser = argparse.ArgumentParser(description='Fortify Remote Client')
     parser.add_argument('-o', '--output', type=Path, default=BASE_PATH.joinpath('output.fvdl'),
                         help='output file')
-    parser.add_argument('-t', '--target', type=Path, default=PARENT_BASE,
-                        help=f'Path for analyzing. Default {PARENT_BASE}')
+    parser.add_argument('-t', '--target', type=str, help='Path for analyzing.')
     parser.add_argument('-ht', '--host', type=str, default='localhost',
                         help='Server host. Default localhost')
     parser.add_argument('-p', '--port', type=int, default=33333,
--- core/data/config.py
+++ core/data/config.py
@@ -2,6 +2,4 @@ from pathlib import Path
 
 BASE_PATH = Path(__file__).parents[2]
 
-PARENT_BASE = BASE_PATH.parents[1]
-
 SLEEP_TIME_NO_RESPONSE = 60
