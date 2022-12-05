f9a163b9b9298b70507c41ce627fb5903eafbcda
zeinlol <nickolaiborshchov@gmail.com>
2022-12-05 16:17:23 +0000

    fix type error

--- core/cli_arguments.py
+++ core/cli_arguments.py
@@ -8,7 +8,7 @@ def create_parser():
     parser = argparse.ArgumentParser(description='Fortify Remote Client')
     parser.add_argument('-o', '--output', type=Path, default=BASE_PATH.joinpath('output.fvdl'),
                         help='output file')
-    parser.add_argument('-t', '--target', type=str, help='Path for analyzing.')
+    parser.add_argument('-t', '--target', type=Path, help='Path for analyzing.')
     parser.add_argument('-ht', '--host', type=str, default='localhost',
                         help='Server host. Default localhost')
     parser.add_argument('-p', '--port', type=int, default=33333,
